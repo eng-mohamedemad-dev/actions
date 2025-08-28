a841a280a98084a186220a6c6bc1be7fb3b50350
eng-mohamedemad-dev <mohamedelghool671@gmail.com>
2025-08-28 13:58:54 +0300

    test

--- routes/web.php
+++ routes/web.php
@@ -120,6 +120,3 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
