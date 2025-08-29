8d56baf31a51d0ef6a287750771f60a8cbf349d1
eng-mohamedemad-dev <mohamedelghool671@gmail.com>
2025-08-29 10:19:52 +0300

    after testing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,5 +22,6 @@
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
       <h1 style="color: red">{{ $name }}</h1>
       <h2 style="color: red">name</h2>
+      <h3 style="background-color: green">welcome after test</h3>
     </body>
 </html>
